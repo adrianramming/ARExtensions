63ae3ee9aa95550b4aa96e1cfa61b62f60e02604
adrianramming <adrian.rammming@me.com>
2014-04-16 11:10:14 +0200

    initial

---
+++ UIColor+Extension.h
@@ -0,0 +1,12 @@
+//
+// Created by Adrian Ramming on 16.04.14.
+// Copyright (c) 2014 init. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor (Extension)
+
+-(UIColor *)initColor;
+
+@end
---
+++ UIColor+Extension.m
@@ -0,0 +1,16 @@
+//
+// Created by Adrian Ramming on 16.04.14.
+// Copyright (c) 2014 init. All rights reserved.
+//
+
+#import "UIColor+Extension.h"
+
+
+@implementation UIColor (Extension)
+
+- (UIColor *)initColor {
+    return [UIColor colorWithRed:230.0f/255.0f green:90.0f/255.0f blue:10.0f/255.0f
+                           alpha:1.0f];
+}
+
+@end
